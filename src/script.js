896a178caa12497d6b7e3750a72bb85b75e96700
Papamus <62795338+Papamus@users.noreply.github.com>
2023-01-25 19:30:22 +0100

    Banner, reponsive svgs, calculator

--- src/script.js
+++ src/script.js
@@ -5,3 +5,85 @@ function popupFunction(){
         window.location.href='dla-klienta.html';
     };
 }
+
+function updateTextInput(val){
+    document.getElementById('textInput').value = val;
+}
+
+function countMyMoney(){
+    let frontendMoney = 50;
+    let backendMoney = 50;
+    let seoMoney = 30;
+    let fcheckbox = document.getElementById('flexCheckFirst');
+    let scheckbox = document.getElementById('flexCheckSecond');
+    let tcheckbox = document.getElementById('flexCheckThird');
+
+    if (fcheckbox.checked){
+        if (scheckbox.checked){
+            if(tcheckbox.checked){
+                let multiplicator = document.getElementById('textInput').value;
+                document.getElementById('finalValue').value = (frontendMoney+backendMoney+seoMoney)*multiplicator*4;
+            }
+            else{
+                let multiplicator = document.getElementById('textInput').value;
+                document.getElementById('finalValue').value = (frontendMoney+backendMoney)*multiplicator*4;
+            }
+        }
+        else if(tcheckbox.checked){
+            let multiplicator = document.getElementById('textInput').value;
+            document.getElementById('finalValue').value = (frontendMoney+seoMoney)*multiplicator*4;
+        }
+        else{
+            let multiplicator = document.getElementById('textInput').value;
+            document.getElementById('finalValue').value = frontendMoney*multiplicator*4;
+        }
+        
+    }
+    if (scheckbox.checked){
+        if (fcheckbox.checked){
+            if(tcheckbox.checked){
+                let multiplicator = document.getElementById('textInput').value;
+                document.getElementById('finalValue').value = (frontendMoney+backendMoney+seoMoney)*multiplicator*4;
+            }
+            else{
+                let multiplicator = document.getElementById('textInput').value;
+                document.getElementById('finalValue').value = (frontendMoney+backendMoney)*multiplicator*4;
+            }
+        }
+        else if(tcheckbox.checked){
+            let multiplicator = document.getElementById('textInput').value;
+            document.getElementById('finalValue').value = (seoMoney+backendMoney)*multiplicator*4;
+        }
+        else{
+            let multiplicator = document.getElementById('textInput').value;
+            document.getElementById('finalValue').value = backendMoney*multiplicator*4;
+        }
+        
+    }
+    if (tcheckbox.checked){
+        if (fcheckbox.checked){
+            if(scheckbox.checked){
+                let multiplicator = document.getElementById('textInput').value;
+                document.getElementById('finalValue').value = (frontendMoney+backendMoney+seoMoney)*multiplicator*4;
+            }
+            else{
+                let multiplicator = document.getElementById('textInput').value;
+                document.getElementById('finalValue').value = (frontendMoney+seoMoney)*multiplicator*4;
+            }
+        }
+        else if(scheckbox.checked){
+            let multiplicator = document.getElementById('textInput').value;
+            document.getElementById('finalValue').value = (seoMoney+backendMoney)*multiplicator*4;
+        }
+        else{
+            let multiplicator = document.getElementById('textInput').value;
+            document.getElementById('finalValue').value = seoMoney*multiplicator*4;
+        }
+        
+    }
+    if (fcheckbox.checked == false && scheckbox.checked == false && tcheckbox.checked == false){
+        document.getElementById('finalValue').value = "Zaznacz usługę!";
+    }
+}
+
+
